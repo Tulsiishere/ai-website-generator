ac5cf69a6f35b70f08107444b91a1684a1accf3f
Tulsiishere <itsmetulsihere@gmail.com>
2026-03-19 20:09:02 +0530

    Add frontend UI, MongoDB, responsive CSS

--- python_check.py
+++ python_check.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+client = genai.Client(api_key=os.getenv("AIzaSyDI9hybJ9SQ5R6dqHAOHuHB2zh1zDs8iPs"))
 
 for model in client.models.list():
     print(model.name)
